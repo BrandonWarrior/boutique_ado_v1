50c2a4fcc56dab7ca2039055f9dd66382c20d090
Brandon Warrior <brandonwarrior@MacBookPro.lan>
2025-04-15 18:16:11 +0100

    completed home page and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -20,18 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^_40kijj^f*77%xy5b)mc%&gozs)d1ewck2f&uz@74zf(yw^y='
+SECRET_KEY = 'django-insecure-bf_nx7m0sd(kbofoqv1g*ba))$ux)2(ju#mr-kr5#21@5iql_d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Redirect URLs after login and logout
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
 
 # Application definition
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',  # Required for django-allauth
-    'allauth',  # django-allauth package
-    'allauth.account',  # Account management (registration, login, etc.)
-    'allauth.socialaccount',  # Social authentication (Google, Facebook, etc.)
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
     'home',
 ]
 
@@ -58,53 +57,52 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'boutique_ado.urls'
 
-# Templating system (ensure 'context_processors.request' is in place for allauth)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-                    os.path.join(BASE_DIR, 'templates'),  # Custom project templates
-                    os.path.join(BASE_DIR, 'home/templates'),  # Home app templates
-                    os.path.join(BASE_DIR, 'templates/allauth'), 
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # Allauth requires this
-                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.request', # required by allauth
+                'django.contrib.auth.context_processors.auth', 
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
 
-# Authentication backends (django-allauth & default django backend)
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',  # Default authentication backend
-    'allauth.account.auth_backends.AuthenticationBackend',  # Allauth authentication backend
-)
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 
-# SITE_ID is required for django-allauth
 SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-ACCOUNT_AUTHENTICATED_REDIRECT_URL = '/'  # Redirect users after login
-ACCOUNT_EMAIL_REQUIRED = True  # Make email required for registration
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # Email verification is mandatory
-ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = '/'  # Redirect users to home after email confirmation
-ACCOUNT_USERNAME_REQUIRED = False  # Allow login with email instead of username
-ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True  # Require users to enter email twice to avoid typos
-ACCOUNT_MINIMUM_USERNAME_LENGTH = 4  # Set minimum length for usernames (if you allow usernames)
-LOGIN_REDIRECT_URL = '/'  # Temporary redirect URL after login
 
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 
-# Database settings (SQLite for development)
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -112,7 +110,10 @@ DATABASES = {
     }
 }
 
-# Password validation settings
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -128,27 +129,31 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Localization settings
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-# Allauth settings (Optional, but useful)
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # For email verification after registration
-ACCOUNT_AUTHENTICATED_REDIRECT_URL = '/'  # Redirect users after logging in
-
-# The following settings are useful for customization
-# You can change them as needed, these are just defaults.
-ACCOUNT_USERNAME_REQUIRED = False  # Allow login via email only
-ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True  # Require email confirmation during signup
