0704737a454ac292c219e713565138749a401e29
Brandon Warrior <brandonwarrior@MacBookPro.lan>
2025-05-07 18:50:19 +0100

    updated webhook hndler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,9 +1,12 @@
-import stripe
 from django.http import HttpResponse
+
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
+
 import json
 import time
+
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -28,28 +31,34 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        # Get the Charge object from the PaymentIntent
-        stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
-
-        # Get billing details from the charge object
-        billing_details = stripe_charge.billing_details
-
-        # Get shipping details from the intent
+        billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-
-        # Get the grand total from the charge amount
-        grand_total = round(stripe_charge.amount / 100, 2)
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
             try:
-                # Check if the order already exists in the database
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
                     email__iexact=billing_details.email,
@@ -69,7 +78,6 @@ class StripeWH_Handler:
             except Order.DoesNotExist:
                 attempt += 1
                 time.sleep(1)
-        
         if order_exists:
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
@@ -77,9 +85,9 @@ class StripeWH_Handler:
         else:
             order = None
             try:
-                # If the order doesn't exist, create a new order
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -92,12 +100,9 @@ class StripeWH_Handler:
                     original_bag=bag,
                     stripe_pid=pid,
                 )
-                
-                # Loop through the bag to add line items
                 for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
-                        # If item_data is just a number, it's the quantity
                         order_line_item = OrderLineItem(
                             order=order,
                             product=product,
@@ -105,7 +110,6 @@ class StripeWH_Handler:
                         )
                         order_line_item.save()
                     else:
-                        # If there are different sizes, loop through them
                         for size, quantity in item_data['items_by_size'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
@@ -115,7 +119,6 @@ class StripeWH_Handler:
                             )
                             order_line_item.save()
             except Exception as e:
-                # If there is an error, delete the order
                 if order:
                     order.delete()
                 return HttpResponse(
